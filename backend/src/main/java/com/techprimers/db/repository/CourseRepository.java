f7b5f4770980ed063beefd50d31aced1e017a79f
方舟 <fangzhou@lawn-143-215-90-200.lawn.gatech.edu>
2019-04-23 15:56:06 -0400

    customer update

--- backend/src/main/java/com/techprimers/db/repository/CourseRepository.java
+++ backend/src/main/java/com/techprimers/db/repository/CourseRepository.java
@@ -3,5 +3,5 @@ package com.techprimers.db.repository;
 import com.techprimers.db.model.TCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<TCourse, String> {
+public interface CourseRepository extends JpaRepository<TCourse, Integer> {
 }
